546b244fbb0d46f288be233587e5ea9622a79bc2
tanjillimon04 <tanjillimon04@gmail.com>
2021-10-13 00:53:40 +0600

    uploading all file
    first commit

---
+++ amar/src/amar/Ami.java
@@ -0,0 +1,16 @@
+package amar;
+import java.util.Scanner;
+public class Ami {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner = new Scanner(System.in);
+int p,i;
+p = scanner.nextInt();
+for(i=5;i>=p;i--){
+	System.out.println("the number is "+i);
+	
+}
+	}
+
+}
